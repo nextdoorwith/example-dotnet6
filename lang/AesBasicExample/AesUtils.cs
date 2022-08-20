ef292ff6275265e6d3b0640d4458f42304919874
nextdoorwith <nextdoorwithadm@gmail.com>
2022-08-20 17:10:52 +0900

    add AES example

---
+++ lang/AesBasicExample/AesUtils.cs
@@ -0,0 +1,129 @@
+﻿using System.Security.Cryptography;
+
+namespace AesBasicExample
+{
+    public static class AesUtils
+    {
+        private const int KeySize = 256;
+        private const int KeyBytes = KeySize / 8;
+
+        /// <summary>
+        /// PBKDF2用ソルト長
+        /// </summary>
+        // NIST(800-132)では、128ビットを推奨
+        private const int Pbkdf2SaltBytes = 128 / 8;
+
+        /// <summary>
+        /// PBKDF2用イテレーション回数
+        /// </summary>
+        // NIST(800-132)では、最小1,000回、できれば10,000,000回を推奨
+        // OWASPではSHA256の場合、310,000回を推奨
+        private const int Pbkdf2Iteration = 1000; // 既定は1,000回
+
+        /// <summary>
+        /// ランダムなキーを生成する。
+        /// </summary>
+        /// <returns>キー</returns>
+        public static byte[] CreateKey()
+        {
+            // 既定では256ビット(32バイト)
+            using var aes = CreateInstance();
+            aes.GenerateKey();
+            return aes.Key;
+        }
+
+        /// <summary>
+        /// 初期ベクトルを生成する。
+        /// </summary>
+        /// <returns>初期ベクトル</returns>
+        public static byte[] CreateIv()
+        {
+            // AES(CBC)ではブロック長と同じ128ビット(64バイト)
+            // ※ブロック長はAESの仕様として128ビット固定
+            using var aes = CreateInstance();
+            aes.GenerateIV();
+            return aes.IV;
+        }
+
+        /// <summary>
+        /// パスワード用のソルトを生成する。
+        /// </summary>
+        /// <returns>ソルト</returns>
+        public static byte[] CreatePasswordSalt()
+        {
+            // 予測が難しい暗号用乱数ジェネレータを使用する必要あり。
+            // (RNGCryptoServiceProviderは非推奨になった)
+            return RandomNumberGenerator.GetBytes(Pbkdf2SaltBytes);
+        }
+
+        /// <summary>
+        /// パスワードからキーを生成する。
+        /// </summary>
+        /// <param name="password">パスワード</param>
+        /// <param name="passwordSalt">パスワード用ソルト</param>
+        /// <returns>キー</returns>
+        public static byte[] CreateKeyFromPassword(string password, byte[] passwordSalt)
+        {
+            // 内部で使用するハッシュ関数として、NIST推奨で広く使われるSHA-256を使用
+            // (既定はSHA-1だが安全性が低いため。CA5379も参考のこと。)
+            var key = new Rfc2898DeriveBytes(password, passwordSalt,
+                Pbkdf2Iteration, HashAlgorithmName.SHA256);
+            return key.GetBytes(KeyBytes);
+        }
+
+        public static byte[] Encrypt(byte[] cleartext, byte[] key, byte[] iv)
+        {
+            using var aes = CreateInstance();
+            aes.Key = key;
+            aes.IV = iv;
+            using var encryptor = aes.CreateEncryptor();
+            return encryptor.TransformFinalBlock(cleartext, 0, cleartext.Length);
+        }
+
+        public static byte[] Decrypt(byte[] ciphertext, byte[] key, byte[] iv)
+        {
+            using var aes = CreateInstance();
+            aes.Key = key;
+            aes.IV = iv;
+            using var decryptor = aes.CreateDecryptor();
+            return decryptor.TransformFinalBlock(ciphertext, 0, ciphertext.Length);
+        }
+
+        // ストリーム操作
+
+        public static void Encrypt(Stream output, byte[] cleartext, byte[] key, byte[] iv)
+        {
+            using var aes = CreateInstance();
+            aes.Key = key;
+            aes.IV = iv;
+            using var encryptor = aes.CreateEncryptor();
+            using var cryptoStream = new CryptoStream(output, encryptor, CryptoStreamMode.Write);
+            cryptoStream.Write(cleartext, 0, cleartext.Length);
+            cryptoStream.Flush();
+        }
+
+        public static byte[] Decrypt(Stream input, byte[] key, byte[] iv)
+        {
+            using var aes = CreateInstance();
+            aes.Key = key;
+            aes.IV = iv;
+            using var decryptor = aes.CreateDecryptor();
+            using var cryptoStream = new CryptoStream(input, decryptor, CryptoStreamMode.Read);
+            using var output = new MemoryStream();
+            cryptoStream.CopyTo(output);
+            return output.ToArray();
+        }
+
+        // その他
+
+        private static Aes CreateInstance()
+        {
+            var aes = Aes.Create();
+            aes.BlockSize = 128; // 既定値(AESの有効なブロックサイズは128bit)
+            aes.KeySize = KeySize; // 既定値は256(128, 192, 256bitを使用可)
+            aes.Mode = CipherMode.CBC; // 既定値
+            aes.Padding = PaddingMode.PKCS7; // 既定値
+            return aes;
+        }
+    }
+}
---
+++ lang/AesBasicExample/AesUtilsTest.cs
@@ -0,0 +1,135 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AesBasicExample
+{
+    public class AesUtilsTest
+    {
+        private const string TestText = @"１２３４５６７８９０壱弐参四五六七八九〇";
+        private static readonly byte[] TestCleartext = Encoding.UTF8.GetBytes(TestText);
+
+        private readonly ITestOutputHelper _logger;
+
+        public AesUtilsTest(ITestOutputHelper logger)
+            => _logger = logger;
+
+        [Fact(DisplayName = "生成されたキーは256ビットであること。")]
+        public void CreateKeyTest()
+        {
+            Assert.True(AesUtils.CreateKey().Length == (256/8));
+        }
+
+        [Fact(DisplayName = "生成された初期ベクトルは128ビットであること。")]
+        public void CreateIvTest()
+        {
+            Assert.True(AesUtils.CreateIv().Length == (128 / 8));
+        }
+
+        [Fact(DisplayName = "生成されたソルトは128ビットであること。")]
+        public void CreatePasswordSaltTest()
+        {
+            Assert.True(AesUtils.CreatePasswordSalt().Length == (128/8));
+        }
+
+        [Fact(DisplayName = "生成されたキーは256ビットであること。")]
+        public void CreateKeyFromPasswordTest()
+        {
+            var salt = AesUtils.CreatePasswordSalt();
+            Assert.True(AesUtils.CreateKeyFromPassword("password", salt).Length == (256/8));
+        }
+
+        [Fact(DisplayName = "パスワードからキー生成の性能評価")]
+        public void CreateKeyFromPasswordPerformanceTest()
+        {
+            const int MaxCount = 10_000;
+            var sw = new Stopwatch();
+            sw.Start();
+            for(var i=0; i< MaxCount; i++)
+            {
+                var salt = AesUtils.CreatePasswordSalt();
+                _ = AesUtils.CreateKeyFromPassword("password", salt);
+            }
+            sw.Stop();
+
+            TimeSpan ts = sw.Elapsed;
+            string elapsedTime = String.Format("({0:n0}) {1:00}:{2:00}:{3:00}.{4:00}",
+                MaxCount,
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            _logger.WriteLine("RunTime " + elapsedTime);
+        }
+
+        [Fact(DisplayName ="暗号化・復号化できること。(バイト列)")]
+        public void EncryptDecryptByBytesTest()
+        {
+            var key = AesUtils.CreateKey();
+            var iv = AesUtils.CreateIv();
+
+            var ciphertext = AesUtils.Encrypt(TestCleartext, key, iv);
+            var decrypted = AesUtils.Decrypt(ciphertext, key, iv);
+
+            Assert.True(ciphertext.Length % (128 / 8) == 0);
+            Assert.Equal(TestText, Encoding.UTF8.GetString(decrypted));
+        }
+
+        [Fact(DisplayName = "暗号化・復号化できること。(ストリーム)")]
+        public void EncryptDecryptByStreamTest()
+        {
+            var key = AesUtils.CreateKey();
+            var iv = AesUtils.CreateIv();
+
+            using var output = new MemoryStream();
+            AesUtils.Encrypt(output, TestCleartext, key, iv);
+            using var input = new MemoryStream(output.ToArray());
+            var decrypted = AesUtils.Decrypt(input, key, iv);
+
+            Assert.Equal(TestText, Encoding.UTF8.GetString(decrypted));
+        }
+
+        [Fact(DisplayName = "暗号化・復号化できること。(バイト列・ストリーム)")]
+        public void EncryptDecryptBytesStreamTest()
+        {
+            var key = AesUtils.CreateKey();
+            var iv = AesUtils.CreateIv();
+
+            var ciphertext = AesUtils.Encrypt(TestCleartext, key, iv);
+            using var input = new MemoryStream(ciphertext);
+            var decrypted = AesUtils.Decrypt(input, key, iv);
+
+            Assert.True(ciphertext.Length % (128 / 8) == 0);
+            Assert.Equal(TestText, Encoding.UTF8.GetString(decrypted));
+        }
+
+        [Fact(DisplayName = "暗号化・復号化できること。(ストリーム・バイト列)")]
+        public void EncryptDecryptStreamBytesTest()
+        {
+            var key = AesUtils.CreateKey();
+            var iv = AesUtils.CreateIv();
+
+            using var output = new MemoryStream();
+            AesUtils.Encrypt(output, TestCleartext, key, iv);
+            var ciphertext = output.ToArray();
+            var decrypted = AesUtils.Decrypt(ciphertext, key, iv);
+
+            Assert.True(ciphertext.Length % (128 / 8) == 0);
+            Assert.Equal(TestText, Encoding.UTF8.GetString(decrypted));
+        }
+
+        [Fact(DisplayName = "パスワードから暗号化・復号化できること。")]
+        public void EncryptDecryptByPasswordTest()
+        {
+            var salt = AesUtils.CreatePasswordSalt();
+            var key = AesUtils.CreateKeyFromPassword("password", salt);
+            var iv = AesUtils.CreateIv();
+
+            var ciphertext = AesUtils.Encrypt(TestCleartext, key, iv);
+            var decrypted = AesUtils.Decrypt(ciphertext, key, iv);
+
+            Assert.True(ciphertext.Length % (128 / 8) == 0);
+            Assert.Equal(TestText, Encoding.UTF8.GetString(decrypted));
+        }
+
+    }
+}
