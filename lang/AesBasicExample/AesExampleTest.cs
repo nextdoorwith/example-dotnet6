b23756649e53dcadb5c8bfeceb1fd8a75b4175ca
nextdoorwith <nextdoorwithadm@gmail.com>
2022-08-26 00:08:35 +0900

    exclude unnecessary code

--- lang/AesBasicExample/AesExampleTest.cs
+++ lang/AesBasicExample/AesExampleTest.cs
@@ -31,40 +31,6 @@ namespace AesBasicExample
             Assert.True(AesExample.CreateIv().Length == (128 / 8));
         }
 
-        [Fact(DisplayName = "生成されたソルトは128ビットであること。")]
-        public void CreatePasswordSaltTest()
-        {
-            Assert.True(AesExample.CreatePasswordSalt().Length == (128/8));
-        }
-
-        [Fact(DisplayName = "生成されたキーは256ビットであること。")]
-        public void CreateKeyFromPasswordTest()
-        {
-            var salt = AesExample.CreatePasswordSalt();
-            Assert.True(AesExample.CreateKeyFromPassword("password", salt).Length == (256/8));
-        }
-
-        [Fact(DisplayName = "パスワードからキー生成の性能評価")]
-        public void CreateKeyFromPasswordPerformanceTest()
-        {
-            const int MaxCount = 10_000;
-            var sw = new Stopwatch();
-            sw.Start();
-            for(var i=0; i< MaxCount; i++)
-            {
-                var salt = AesExample.CreatePasswordSalt();
-                _ = AesExample.CreateKeyFromPassword("password", salt);
-            }
-            sw.Stop();
-
-            TimeSpan ts = sw.Elapsed;
-            string elapsedTime = String.Format("({0:n0}) {1:00}:{2:00}:{3:00}.{4:00}",
-                MaxCount,
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
-            _logger.WriteLine("RunTime " + elapsedTime);
-        }
-
         [Fact(DisplayName = "暗号化・復号化できること。(ストリーム)")]
         public void EncryptDecryptByStreamTest()
         {
@@ -121,20 +87,6 @@ namespace AesBasicExample
             using var output = new MemoryStream();
             AesExample.Encrypt(output, TestCleartext, key, iv);
             var ciphertext = output.ToArray();
-            var decrypted = AesExample.Decrypt(ciphertext, key, iv);
-
-            Assert.True(ciphertext.Length % (128 / 8) == 0);
-            Assert.Equal(TestText, Encoding.UTF8.GetString(decrypted));
-        }
-
-        [Fact(DisplayName = "パスワードから暗号化・復号化できること。")]
-        public void EncryptDecryptByPasswordTest()
-        {
-            var salt = AesExample.CreatePasswordSalt();
-            var key = AesExample.CreateKeyFromPassword("password", salt);
-            var iv = AesExample.CreateIv();
-
-            var ciphertext = AesExample.Encrypt(TestCleartext, key, iv);
             var decrypted = AesExample.Decrypt(ciphertext, key, iv);
 
             Assert.True(ciphertext.Length % (128 / 8) == 0);
