9a3e87ffa634f52dde2d4215fcbd3615ed1c4829
nextdoorwith <nextdoorwithadm@gmail.com>
2022-09-08 21:14:40 +0900

    update pbkdf2 performance

--- lang/KdfBasicExample/Pbkdf2Example.cs
+++ lang/KdfBasicExample/Pbkdf2Example.cs
@@ -20,9 +20,9 @@ namespace KdfBasicExample
         /// パスワードからキーを生成する。
         /// </summary>
         /// <param name="password">パスワード</param>
-        /// <param name="passwordSalt">パスワード用ソルト</param>
+        /// <param name="salt">パスワード用ソルト</param>
         /// <returns>キー</returns>
-        public static byte[] CreateKeyFromPassword(string password, byte[] passwordSalt)
+        public static byte[] CreateKeyFromPassword(string password, byte[] salt)
         {
             // PBKDF2を使用してキーを生成
             // - イテレーション回数
@@ -31,13 +31,22 @@ namespace KdfBasicExample
             //   - Microsoftでは、100,000回以上を推奨(コード分析: CA5387)
             // - 内部で使用するハッシュ関数として、NIST推奨で広く使われるSHA-256を使用
             //   - 既定はSHA-1だが安全性が低い(コード分析: CA5379)
+#if true
+            return Rfc2898DeriveBytes.Pbkdf2(
+                password,
+                salt,
+                310_000,
+                HashAlgorithmName.SHA256,
+                256 / 8);
+#else
             using var key = new Rfc2898DeriveBytes(
                 password,
-                passwordSalt,
+                salt,
                 310_000,
                 HashAlgorithmName.SHA256
             );
             return key.GetBytes(256/8);
+#endif
         }
 
     }
--- lang/KdfBasicExample/Program.cs
+++ lang/KdfBasicExample/Program.cs
@@ -4,5 +4,7 @@ Console.WriteLine("-----");
 {
     var salt = Pbkdf2Example.CreatePasswordSalt();
     var key = Pbkdf2Example.CreateKeyFromPassword("password", salt);
-    Console.WriteLine($"{Convert.ToHexString(key)}({key.Length})");
+
+    Console.WriteLine($"IV : {Convert.ToHexString(salt)}({salt.Length})");
+    Console.WriteLine($"Key: {Convert.ToHexString(key)}({key.Length})");
 }
