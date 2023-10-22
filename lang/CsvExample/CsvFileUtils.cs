3e35cecae5613b90896f301cb47b946a2b93a843
nextdoorwith <nextdoorwithadm@gmail.com>
2023-10-22 18:56:46 +0900

    add CsvExample

---
+++ lang/CsvExample/CsvFileUtils.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+
+namespace CsvExample
+{
+    public class CsvFileUtils
+    {
+        /// <summary>
+        /// CSVファイルの内容を解析する。
+        /// </summary>
+        /// <param name="reader">CSVファイル内容</param>
+        /// <returns>文字列配列リスト</returns>
+        /// <exception cref="ArgumentNullException">readerがnullの場合</exception>
+        /// <exception cref="FormatException">終端の引用符がない場合</exception>
+        public static List<string[]> Parse(TextReader reader)
+        {
+            _ = reader ?? throw new ArgumentNullException(nameof(reader));
+
+            var rowList = new List<string[]>();
+            string val; int deli;
+            do
+            {
+                // １行分のカラム値を取得
+                var colList = new List<string>();
+                do
+                {
+                    // 区切り文字・行終端(または引用符開始)までの値を取得
+                    (val, deli) = ScanValue(reader, c => c == ',' || c == '"' || c == '\r' || c == '\n');
+
+                    // 引用符だった場合、次の引用符までの値を取得
+                    if (deli == '"')
+                    {
+                        // 次の引用符までを値とする。
+                        // 終端の引用符が存在しない場合は例外とする。
+                        (val, deli) = ScanValue(reader, c => c == '"');
+                        if (deli != '"') throw new FormatException("no ending quotation");
+
+                        // 終端となる引用符の次にある区切り文字まで読み飛ばす
+                        (_, deli) = ScanValue(reader, c => c == ',' || c == '\r' || c == '\n');
+                    }
+
+                    // 取得した値をカラム値リストに追加
+                    colList.Add(val);
+
+                } while (deli == ','); // 区切り文字の場合、次のカラム値を取得
+
+                // 改行読み飛ばし(\r\n時は追加で\nを読み飛ばし)
+                if (deli == '\r' && reader.Peek() == '\n') deli = reader.Read();
+
+                rowList.Add(colList.ToArray());
+
+            } while (deli >= 0); // ファイル終端(-1)でない場合は繰り返し
+
+            return rowList;
+        }
+
+        /// <summary>
+        /// 停止条件まで値を読み取る
+        /// </summary>
+        /// <param name="reader">リーダー</param>
+        /// <param name="stopFunc">停止条件</param>
+        /// <returns>対象行に含まれる値群</returns>
+        private static (string, int) ScanValue(TextReader reader, Func<int, bool> stopFunc)
+        {
+            StringBuilder sb = new StringBuilder();
+            int ch;
+            while ((ch = reader.Read()) >= 0)
+            {
+                // 連続する引用符は単一の引用符と解釈
+                if (ch == '"' && reader.Peek() == '"')
+                {
+                    ch = reader.Read();
+                    sb.Append((char)ch);
+                    continue;
+                }
+
+                if (stopFunc(ch)) break;
+
+                sb.Append((char)ch);
+            }
+            return (sb.ToString(), ch);
+        }
+    }
+}
---
+++ lang/CsvExample/CsvFileUtilsTest.cs
@@ -0,0 +1,90 @@
+﻿using Xunit;
+
+namespace CsvExample
+{
+    public class CsvFileUtilsTest
+    {
+        [Fact(DisplayName = "引数readerがnullの場合、例外が発生すること。")]
+        public void Parse_Arg_Reader_Null()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => CsvFileUtils.Parse(null));
+            Assert.Equal("Value cannot be null. (Parameter 'reader')", ex.Message);
+        }
+
+        [Fact(DisplayName = "引用符の閉じ忘れがある場合、例外が発生すること。")]
+        public void Parse_NoEndingQuote()
+        {
+            var reader = new StringReader("abc,\"def,ghi\r\njkl");
+            var ex = Assert.Throws<FormatException>(() => CsvFileUtils.Parse(reader));
+            Assert.Equal("no ending quotation", ex.Message);
+        }
+
+        [Theory(DisplayName = "CSVファイルのバリエーション")]
+        [MemberData(nameof(ParseVariation))]
+        public void Parse_Variation(string csv, List<string[]> expected)
+        {
+            var reader = new StringReader(csv);
+            var actual = CsvFileUtils.Parse(reader);
+            Assert.Equal(expected, actual);
+        }
+
+        private static List<string[]> ToList(params string[][] s2) => s2.ToList();
+        private static string[] ToR(params string[] s1) => s1;
+        private static readonly string[] EmpRow = new string[] {""};
+        public static object[][] ParseVariation = new object[][]
+        {
+            // ★基本
+            new object[]{ "", ToList(ToR("")) },
+            new object[]{ "a", ToList(ToR("a")) },
+            new object[]{ "a,", ToList(ToR("a", "")) },
+            new object[]{ "a,b", ToList(ToR("a", "b")) },
+            new object[]{ "a,,c", ToList(ToR("a", "", "c")) },
+            new object[]{ " a , b , c ", ToList(ToR(" a ", " b ", " c ")) },
+            new object[]{ "いち,に,さん", ToList(ToR("いち", "に", "さん")) },
+            new object[]{ "\n,\n,,", ToList(ToR(""), ToR("", ""), ToR("", "", "")) },
+            // ★引用符
+            new object[]{ "\"a1\"", ToList(ToR("a1")) },
+            new object[]{ "\"a1\",b", ToList(ToR("a1", "b")) },
+            new object[]{ " \"a1\" ,b", ToList(ToR("a1", "b")) },
+            new object[]{ "\"a1,a2\",b", ToList(ToR("a1,a2", "b")) },
+            new object[]{ "\"a1\"\"a2\",b", ToList(ToR("a1\"a2", "b")) },
+            new object[]{ "\"a1\"\",\r\na2\",b", ToList(ToR("a1\",\r\na2", "b")) },
+            new object[]{ "a,\"b1\r\nb2\",c", ToList(ToR("a", "b1\r\nb2", "c")) },
+            new object[]{ "a,b,\"c1\r\nc2\"", ToList(ToR("a", "b", "c1\r\nc2")) },
+            // ★引用符（引用符前後の冗長文字をスキップ）
+            new object[]{ " aaa\"a1\ra2\"a\"\"aa", ToList(ToR("a1\ra2")) },
+            new object[]{ "a,b, ccc\"c1\r\nc2\"cc\"\"c", ToList(ToR("a", "b", "c1\r\nc2")) },
+            // ★改行
+            new object[]{ "a,b,c\r", ToList(ToR("a", "b", "c"), EmpRow) },
+            new object[]{ "a,b,c\n", ToList(ToR("a", "b", "c"), EmpRow) },
+            new object[]{ "a,b,c\r\n", ToList(ToR("a", "b", "c"), EmpRow) },
+            new object[]{ "a,b,c\r\r", ToList(ToR("a", "b", "c"), EmpRow, EmpRow) },
+            new object[]{ "a,b,c\n\n", ToList(ToR("a", "b", "c"), EmpRow, EmpRow) },
+            new object[]{ "a,b,c\r\n\r\n", ToList(ToR("a", "b", "c"), EmpRow, EmpRow) },
+            new object[]{ "a,b,c\rd,e\n,f,\r\ng",
+                ToList(ToR("a", "b", "c"), ToR("d", "e"), ToR("", "f", ""), ToR("g")) },
+            // ★標準的なCSV
+            new object[]{
+                "col1,col2,col3\r\n" + 
+                "値１－１,値１－２,\r\n" + 
+                ",値２－２,値２－３",
+                ToList(
+                    ToR("col1", "col2", "col3"),
+                    ToR("値１－１", "値１－２", ""),
+                    ToR("", "値２－２", "値２－３")
+                )
+            },
+            new object[]{
+                "col1,col2,col3\r\n" +
+                ",\"ああ\rいい\", \r\n" +
+                "ううう,,\"ええ,\r\nおお\"",
+                ToList(
+                    ToR("col1", "col2", "col3"),
+                    ToR("", "ああ\rいい", " "),
+                    ToR("ううう", "", "ええ,\r\nおお")
+                )
+            },
+        };
+
+    }
+}
